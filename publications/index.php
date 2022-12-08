82930300be04b26ae662f4212d07c3197eee8765
Jeff Carver <carver@cs.ua.edu>
2022-12-08 07:30:26 -0600

    added current website files

---
+++ publications/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>[ Jeffrey C. Carver | All Publications ]</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+<script type="text/javascript" src="js/jquery.tipsy.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox-1.3.4.pack.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<link rel="stylesheet" type="text/css" href="css/custom.css"/>
+<link rel="stylesheet" type="text/css" href="css/tipsy.css"/>
+<link rel="stylesheet" type="text/css" href="css/fancybox.css"/>
+
+<style> .container a {color: #347AB7 !important;} </style>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/menucollapse.js"></script>
+<script type="text/javascript" src="js/arrow78.js"></script><html>
+
+
+<script language="JavaScript">
+<!--
+    function spiderjam(mym, myd){
+        document.write("<a href=mailto:"	+ mym + "&#64;" + myd + ">" + mym + "&#64;" + myd + "</a>");
+    }
+-->
+</script>
+
+<body id="page-top" class="index">
+
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"  data-target="#bs-example-navbar-collapse-2" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="glyphicon glyphicon-search"></span>
+            </button>
+            <button id="button2" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <span><a href="#"><img border="0" width="170" src="../images/UA.png"/ alt=”The University of Alabama logo” style=""></a></span>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="http://carver.cs.ua.edu">
+                Home<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                    <li><a onclick="javascript:reset_menus();$('#tab-1-content').show();" href="http://carver.cs.ua.edu#intro">Introduction</a></li>
+                        <li><a href="http://carver.cs.ua.edu/bio.html">Short Biography</a></li>
+                        <li><a target="_blank" href="http://carver.cs.ua.edu/CV-Jeffrey-Carver.pdf">CV (PDF)</a></li>
+                    </ul>
+                </li>
+				 <li class="page-scroll">
+                    <a onclick="javascript:reset_menus();$('#tab-10-content').show();" href="http://carver.cs.ua.edu/#grants">Grants</a>
+                </li>
+
+                <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                Publications<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                    <li><a onclick="javascript:reset_menus();$('#tab-2-content').show();" href="http://carver.cs.ua.edu/#publications">Selected</a></li>
+                        <li><a href="publications/">All</a></li>
+                    </ul>
+                </li>
+                 <li class="page-scroll">
+                    <a onclick="javascript:reset_menus();$('#tab-3-content').show();" href="http://carver.cs.ua.edu/#awards">Awards</a>
+                </li>
+                <li class="page-scroll">
+                    <a onclick="javascript:reset_menus();$('#tab-4-content').show();" href="http://carver.cs.ua.edu/#editorial">Editorial Boards</a>
+                </li>
+                <li class="page-scroll">
+                    <a onclick="javascript:reset_menus();$('#tab-5-content').show();" href="http://carver.cs.ua.edu/#steering">Steering Committees</a>
+                </li>
+				        <li class="page-scroll">
+                    <a onclick="javascript:reset_menus();$('#tab-6-content').show();" href="http://carver.cs.ua.edu/#courses">Courses</a>
+                </li>
+                <li class="page-scroll">
+                    <a onclick="javascript:reset_menus();$('#tab-7-content').show();" href="http://carver.cs.ua.edu/#conference">Conference Committees</a>
+                </li>
+                <li class="page-scroll">
+                <a onclick="javascript:reset_menus();$('#tab-8-content').show();" href="http://carver.cs.ua.edu/#advising">Advising</a>
+                </li>
+              </ul>
+        </div><!-- /.navbar-collapse -->
+
+
+    </div><!-- /.container-fluid -->
+</nav>
+
+<section>
+    <!-- Place this tag where you want the search results to render -->
+    <gcse:searchresults-only></gcse:searchresults-only>
+</section>
+
+<section id="tree" style="margin-top:50px">
+    <div class="container">
+        <a href="../index.html">Jeffrey Carver</a> > All Publications
+    </div>
+</section>
+
+<!-- Home Section -->
+<section id="home">
+    <div class="container" id="publications">
+    <br>
+    <h1 align="CENTER">All Publications of Jeffrey Carver</h1>
+	<div align="right" width="100%">
+      <a target="_blank" href="https://scholar.google.com/citations?user=qGe7nJ0AAAAJ&hl=en"><img height="40" src="images/scholar.png" alt="Google image"></a> &nbsp;&nbsp;&nbsp;&nbsp;
+	   <a target="_blank" href="https://dblp.org/pid/c/JeffreyCCarver"><img height="40" src="images/dblp.png" alt="dblp image"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+  </div>
+  <div>
+    <p>For each paper below, the URL on the title links to the official publication of record.</p>
+    <p><img src="images/pdf.gif" height="20" alt=”large pdf icon” style=""> provides the link to the preprint (if available).</p>
+    <p><img src="images/ppt.gif" height="20" alt=”large power point icon”  style=""> provides the link to the slides (if available).</p>
+  </div>
+
+<?php
+    error_reporting(E_NONE);
+    ini_set('display_errors', '0');
+
+    /*
+     Available Fields:
+
+     The field TYPE will be used for data-filtering. If it is not available then the item will be
+     marked as uncategorised.
+
+     'note'
+     'abstract'
+     'year'
+     'group'
+     'publisher'
+     'location'
+     'articleno'
+     'numpages'
+     'page-start'
+     'page-end'
+     'pages'
+     'address'
+     'url'
+     'doi'
+     'volume'
+     'chapter'
+     'journal'
+     'author'
+     'raw'
+     'title'
+     'booktitle'
+     'folder'
+     'type'
+     'series'
+     'linebegin'
+     'lineend'
+     'durl',
+     'powerpoint',
+     'infosite',
+     'website'
+
+     */
+
+    /*
+     Define the format that will be used for printing each bibtex item.
+     If a you desire to print a string infront of a field please use the following format:
+
+     article = array("title", "author", "string", "bibtex field");
+
+     eg.
+
+     article = array("title", "author", "Num. Of pages", "pages");
+
+     Please modify the example below as desired  is presented bellow.
+     */
+
+    $article = array("title", "author", "journal", "series", "location", "publisher", "volume", "number", "pages", "address", "isbn", "year","shorthand");
+    $book = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $booklet = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $conference = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year", "award","shorthand");
+    $inbook = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $incollection = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "chapter", "pages", "address", "isbn", "year", "award");
+    $inproceedings = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "chapter", "pages", "address", "isbn", "year", "award");
+    $manual = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $mastersthesis = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $misc = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $phdthesis = array("title", "author", "journal", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $proceedings = array("booktitle", "series", "author", "location", "publisher", "volume", "pages", "address", "isbn", "year", "award","shorthand");
+    $techreport = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $unpublished = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+    $other = array("title", "author", "booktitle", "series", "location", "publisher", "volume", "pages", "address", "isbn", "year");
+
+    /*
+     Delimiter for Seperating each bibtex field
+     */
+
+    $delimiter = '.';
+
+    /*
+
+     Enter fields equivalent to type field in the BibTex file to sort the bibtex entries in categories. Bellow each type enter the title which will be presented as the category title.
+
+     */
+
+    $sortby = array('journal', 'editorial', 'conference', 'book-chap','workshop','phdthesis','techreport');
+    $sortbyTitle = array('Journals', 'Editorially Reviewed Journals/Magazines','Conference Proceedings', 'Book Chapters','Lightly Refereed Workshops and Other Publications','Dissertation','Tech. Report');
+
+    $projects = array('all');
+    include 'bibtex.php';
+
+   /*
+     Enter the location of your BibTex file
+    */
+    $bibTexFile = 'vitae.bib';
+
+    $bibTex = new BibTeX_Parser();
+    $bibTex->parser($file = $bibTexFile);
+    ?>
+</body>
+</html>
